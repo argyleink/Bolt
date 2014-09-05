1b52f1e02a2c2a51774aaaa85c3e3104e1360680
Adam Argyle <atom@argyleink.com>
2014-09-05 11:16:56 -0700

    no more app.js opinions

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,5 @@
 $(function(){
 
-  // Init Fastclick
-  FastClick.attach(document.body);
-
-  // $.ajaxPrefilter('script', function(settings, osettings, xhr) {
-  //   settings.cache = true;
-  // });
-  // $.ajaxSetup({'isLocal': true});
-
-  // set container height
-  // fixes many bugs to have the height set explicitly on a parent of absolute elements
-  // watch it on resize, to reset app height, jqueries resize is throttled
-  $(window).resize(setAppHeight);
-  function setAppHeight() {
-    App.Browser.ipadAndUp = window.innerWidth >= 1024;
-  }
-  setAppHeight();
+  console.log('all is well in the cosmos');
 
 });

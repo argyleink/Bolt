bb99a8b984ca0ecf46e3ec69144d72d409363667
Adam Argyle <atom@argyleink.com>
2014-10-22 14:50:38 -0700

    rendering client template for example

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,23 @@ App.ModuleTemplate = (function(){
     return appName;
   }
 
+  function renderJadeList() {
+    // http://projects.jga.me/clientjade/
+    jade.render(
+      $('#list-template-example')[0],       // DOM Node
+      'list',                               // Template name
+      {
+        items: [
+          { title: 'Title 1', detail: 'Detail 1' },
+          { title: 'Title 2', detail: 'Detail 2' },
+          { title: 'Title 3', detail: 'Detail 3' }
+        ]
+      }
+    );
+  }
+
+  renderJadeList();
+
   return {
     name: publicMethod
   }

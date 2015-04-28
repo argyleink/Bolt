8c2fcbf83aed5e576ea8c931bb02e35dba4bbe8c
Adam Argyle <atom@argyleink.com>
2015-04-28 16:15:45 -0700

    easier app.js file

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+var App = {};
+App.Startup = (function(){
+
+  var appName = 'Jade and Stylus rule';
+
+  Velocity(document.querySelectorAll('#bolt-logo path'), 'transition.expandIn', {
+    drag: true,
+    stagger: 100
+  });
+  
+  window.onload = function(){
+    App.TemplateExample.render();
+  }
+
+})();

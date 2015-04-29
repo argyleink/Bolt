efc97ca1906c82af1a080302e19e35bb0cd25cb2
Adam Argyle <atom@argyleink.com>
2015-04-29 11:12:12 -0700

    delayed intro animation

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,9 @@ App.Startup = (function(){
   var appName = 'Jade and Stylus rule';
 
   Velocity(document.querySelectorAll('#bolt-logo path'), 'transition.expandIn', {
-    drag: true,
-    stagger: 100
+    drag:     true,
+    stagger:  100,
+    delay:    500
   });
   
   window.onload = function(){

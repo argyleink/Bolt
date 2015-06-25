0d7cf3be76c0006fbedb6010ea6ed2d0cec2039c
Adam Argyle <atom@argyleink.com>
2015-06-25 12:10:03 -0700

    es6ify lol

--- server/bower-deps.js
+++ server/bower-deps.js
@@ -1,27 +1,21 @@
-var bower_base = "app/"
+var __dirname = __dirname.substr(0, __dirname.length - 6)
+  , bower_base = "app/"
   , files
-  , replaceBase
 
 files = require("wiredep")({
   exclude: [/jquery/]
 })
 
-__dirname = __dirname.substr(0,__dirname.length - 6)
+files.js = files.js.map((path) => 
+  path.replace(__dirname + bower_base, '')
+)
 
-replaceBase = function(path) {
-  return path.replace(__dirname + bower_base, '')
-}
+files.css = files.css.map((path) =>
+  path.replace(__dirname + bower_base + '', '')
+)
 
-files.js = files.js.map(function(path) {
-  return path.replace(__dirname + bower_base, '')
-})
-
-files.css = files.css.map(function(path) {
-  return path.replace(__dirname + bower_base + '', '')
-})
-
-files.dev_css = files.css.map(function(path) {
-  return path.replace('js/', '')
-})
+files.dev_css = files.css.map((path) =>
+  path.replace('js/', '')
+)
 
 module.exports = files
